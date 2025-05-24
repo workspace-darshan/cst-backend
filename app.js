230564c60ffea43346433be7fc0ccfe7075f3e16
Darshan Mevada <darshan-mevada@mavenprofserv.com>
2025-05-24 15:04:28 +0530

    deplyment changes

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Connect to MongoDB
--- config/constant.js
+++ config/constant.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 exports.dbConfig = {
     url: process.env.MONGODB_URI_LIVE,
-    dbName: process.env.DB_Name,
+    dbName: process.env.DB_NAME,
 };
 
 exports.secretKey = process.env.JWT_SECRET
